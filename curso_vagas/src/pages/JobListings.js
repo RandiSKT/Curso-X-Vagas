37c173fa4cde68f0e858cc46586fabb90faeae23
Fernando Randi Nascimento <980177@queroquero.local>
2023-11-14 12:38:10 -0300

    Adicionando as primeiras informações de vagas de trabalho

--- curso_vagas/src/pages/JobListings.js
+++ curso_vagas/src/pages/JobListings.js
@@ -7,39 +7,45 @@ import './JobListings.css';
 const JobListings = () => {
   const jobListingsData = [
     {
-      title: 'Desenvolvedor Frontend',
-      company: 'Empresa A',
-      requirements: ['HTML', 'CSS', 'JavaScript'],
+      title: 'ESPECIALISTA EM E-MAIL MARKETING = HTML E CSS INLINE',
+      company: 'Leilão Publico',
+      requirements: ['HTML', 'CSS', 'Marketing', 'Pacote Ofice'],
+      responsibilities: 'Anúncios Na Internet, Anúncios Em Sites Classificados E Redes Sociais. Divulgar Mídias Alternativas De Amplo Alcance. Eventualmente Irá Elaborar, Criar, Publicar Flyers Eletrônicos, Banners E Outras Formas De Divulgação Ampla Em Ambiente Web Relacionados Com Vendas Públicas; Administrativo De Escritório Rotinas Internas (Via De Regra). Adobe Illustrator, Word, Excel, Paint, Base Gmail, Google Drive, Google Agenda. ',
     },
     {
-      title: 'Desenvolvedor Frontend',
-      company: 'Empresa A',
-      requirements: ['HTML', 'CSS', 'JavaScript'],
+      title: ' DESIGN E CRIAÇAO BANNERS',
+      company: 'Leilão Publico',
+      requirements: ['Adobe Photoshop', 'Desing', 'Marketing'],
+      responsibilities: 'Anúncios Na Internet, Anúncios Em Sites Classificados E Redes Sociais. Divulgar Mídias Alternativas De Amplo Alcance. Eventualmente Irá Elaborar, Criar, Publicar Flyers Eletrônicos, Banners E Outras Formas De Divulgação Ampla Em Ambiente Web Relacionados Com Vendas Públicas; Administrativo De Escritório Rotinas Internas (Via De Regra). Adobe Illustrator, Word, Excel, Paint, Base Gmail, Google Drive, Google Agenda. ',
     },
     {
-      title: 'Desenvolvedor Frontend',
-      company: 'Empresa A',
-      requirements: ['HTML', 'CSS', 'JavaScript'],
+      title: 'Estágio em Análise de Dados',
+      company: 'Doutorie',
+      requirements: ['SQL', 'Python', 'Pacote Office'],
+      responsibilities: '•  Coletar, selecionar e otimizar dados para atender às necessidades do cliente. \n•  Utilizar insights da análise de dados para melhorar ofertas e processos empresariais. •  Analisar grandes volumes de dados para extrair conhecimento e aprimorar a empresa. •  Apresentar resultados de forma acessível, usando ferramentas de visualização de dados.',
     },
     {
-      title: 'Analista de Dados',
-      company: 'Empresa B',
-      requirements: ['SQL', 'Python', 'Estatística'],
+      title: 'estágio em suporte',
+      company: 'Bendito',
+      requirements: ['Comunicação', 'Gestão da Informação'],
+      responsibilities: '•  Atuar com suporte direto aos clientes; •  Retirar dúvidas dos usuários; •  Documentar erros na ferramenta e reportá-los ao time de produto; •  Participar ativamente da jornada de retenção de clientes; •  Participar da elaboração do roadmap de produto.',
     },
     {
-      title: 'Engenheiro de Software',
-      company: 'Empresa C',
-      requirements: ['Java', 'Spring', 'Banco de Dados'],
+      title: 'estágio em área administrativa',
+      company: 'Facilita',
+      requirements: ['Comunicação', 'Pacote Office', 'Licitações'],
+      responsibilities: '• Realizar pesquisa direcionada nos portais de licitação • Organizar cada oportunidade destinando-as por data de cadastro. • Captar novos portais de licitações.  • Trazer informações de pregões presenciais, cotações, dispensas, compras diretas de forma imediata para o setor comercial. • Selecionar oportunidades dentro do software',
     },
     {
-      title: 'Engenheiro de Software',
-      company: 'Empresa C',
-      requirements: ['Java', 'Spring', 'Banco de Dados'],
+      title: ' Desenvolvimento de Aplicativo',
+      company: 'Facilita',
+      requirements: ['JavaScript', 'React Native', 'POO'],
+      responsibilities: 'Atuar no desenvolvimento de softwares escritos em, mas não limitados a, linguagem Javascript com Framework React Native, programando, codificando, testando, especificando e/ou validando;Pesquisar, estudar e propor novas  tecnologias, funcionalidades, produtos e melhorias. Auxílio em documentação, componentes e telas dos Apps. Empresa brasileira com atuação global, desenvolvedora de soluções de hardware e software para os mercados de telecomunicações, controle de acesso e IoT.',
     },
-    // Adicione mais vagas de emprego conforme necessário
   ];
 
   const [completedDisciplines, setCompletedDisciplines] = useState([]);
+  const [showDetailsIndex, setShowDetailsIndex] = useState(null);
 
   const handleDisciplineToggle = (discipline) => {
     setCompletedDisciplines((prevDisciplines) =>
@@ -51,6 +57,14 @@ const JobListings = () => {
 
   const isDisciplineCompleted = (discipline) => completedDisciplines.includes(discipline);
 
+  const handleShowDetails = (index) => {
+    setShowDetailsIndex(index);
+  };
+
+  const handleHideDetails = () => {
+    setShowDetailsIndex(null);
+  };
+
   return (
     <div>
       <Header />
@@ -60,7 +74,7 @@ const JobListings = () => {
           {jobListingsData.map((job, index) => (
             <div className="job-listing" key={index}>
               <h3>{job.title}</h3>
-              <p>{job.company}</p>
+              <p><strong>Empresa: {job.company}</strong></p>
               <div className="requirements">
                 {job.requirements.map((requirement, i) => (
                   <span
@@ -72,6 +86,18 @@ const JobListings = () => {
                   </span>
                 ))}
               </div>
+              <button className="info-button" onClick={() => handleShowDetails(index)}>
+                + Informações
+              </button>
+              {showDetailsIndex === index && (
+                <div className="job-details">
+                  <p><strong>Responsabilidades:</strong> {job.responsibilities}</p>
+                  {/* Adicione mais informações conforme necessário */}
+                  <button className="hide-details-button" onClick={handleHideDetails}>
+                    Ocultar Detalhes
+                  </button>
+                </div>
+              )}
             </div>
           ))}
         </div>
